691a4246d8e4303558a87c70fee99b66773baf07
afc163 <afc163@gmail.com>
2020-08-12 15:35:07 +0800

    use src/index.ts entry instead of dist/g6.min.js

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,3 +1,2 @@
-// window.g6 = require('./src/index.ts'); // import the source for debugging
-window.g6 = require('./dist/g6.min.js'); // import the package for webworker
+window.g6 = require('./src/index.ts'); // import the source for debugging
 window.insertCss = require('insert-css');
